8c6f27afb0f63397d410f93963ecd3789f94eb4d
Nmsh <85759042+nm1-Dev@users.noreply.github.com>
2025-10-18 23:41:35 +0300

    Delete server/custom/framework directory

--- server/custom/framework/esx.lua
+++
@@ -1,9 +0,0 @@
-if Config.Framework ~= "esx" then
-    return
-end
-
-local ESX = exports['es_extended']:getSharedObject()
-
-function RegisterUseableItem(name, cb)
-    ESX.RegisterUsableItem(name, cb)
-end
--- server/custom/framework/qbcore.lua
+++
@@ -1,9 +0,0 @@
-if Config.Framework ~= "qb" then
-    return
-end
-
-local QBCore = exports['qb-core']:GetCoreObject()
-
-function RegisterUseableItem(name, cb)
-    QBCore.Functions.CreateUseableItem(name, cb)
-end
